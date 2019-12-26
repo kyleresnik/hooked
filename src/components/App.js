4d6400c69e20f1bc50244edf82b2f9450d12df06
Kyle Resnik <kylebresnik@gmail.com>
2019-12-25 21:57:12 -0500

    compiled successfully

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useEffect } from 'react';
+import '../App.css';
 import Header from './Header';
 import Movie from './Movie';
 import Search from './Search';
@@ -8,23 +7,59 @@ import Search from './Search';
 
 const MOVIE_API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=9f3adc34";
 
+const initialState = {
+  loading: true,
+  movies: [],
+  errorMessage: null
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null
+      };
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload
+      };
+    case "SEARCH_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error
+      };
+    default:
+      return state;
+  }
+};
+
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(null);
+  // const [loading, setLoading] = useState(true);
+  // const [movies, setMovies] = useState([]);
+  // const [errorMessage, setErrorMessage] = useState(null);
   // We can use multiple useState functions in one component. The first one
   // is used to handle the loading state and will render "Loading..." when
   // set to True. The second is used to handle the movies array from the
   // server. The third is used to handle any errors that may occur when
   // making a request to the API.
 
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(() => {
     fetch(MOVIE_API_URL)
       .then(response => response.json())
       .then(jsonResponse => {
-        setMovies(jsonResponse.Search);
-        setLoading(false);
+
+      dispatch({
+        type: "SEARCH_MOVIES_SUCCESS",
+        payload: jsonResponse.Search
       });
+    });
   }, []);
   // useEffect hook lets you perform side effects in your function, such as
   // data fetching, subscriptions, and manual DOM manipulations. You can think
@@ -35,21 +70,28 @@ const App = () => {
   // React to skip applying an effect if the value passed in hasn't changed.
 
   const search = searchValue => {
-    setLoading(true);
-    setErrorMessage(null);
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST"
+    });
 
     fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=9f3adc34`)
       .then(response => response.json())
       .then(jsonResponse => {
         if (jsonResponse.Response === "True") {
-          setMovies(jsonResponse.Search);
-          setLoading(false);
+          dispatch({
+            type: "SEARCH_MOVIES_SUCCESS",
+            payload: jsonResponse.Search
+          });
         } else {
-          setErrorMessage(jsonResponse.Error);
-          setLoading(false);
+          dispatch({
+            type: "SEARCH_MOVIES_FAILURE",
+            error: jsonResponse.Error
+          });
         }
       });
   };
+
+  const { movies, errorMessage, loading } = state;
 
   return (
     <div className="App">
